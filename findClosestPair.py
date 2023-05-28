571792fd8d665c91e1721d4b654f8c0798372e05
demelcjohn <demal.john007@gmail.com>
2023-05-28 17:27:25 +0530

    added min distance

--- findClosestPair.py
+++ findClosestPair.py
@@ -8,3 +8,14 @@ def findClosestPair(X, Y):
         return dist(X[0], X[1])
     if (n == 3):
         return min(dist(X[0], X[1]), dist(X[0], X[2]).dist(X[2], X[1]))
+
+    mid = n//2
+
+    dl = findClosestPair(X[:(n//2)][:], Y)
+    dr = findClosestPair(X[(n//2):][:], Y)
+    d = min(dl, dr)
+
+    S = []
+    for i in range(n):
+        if ((X[n//2][0]-d <= Y[0]) and (X[n//2][0]+d >= Y[0])):
+            S = S.append(Y[i])
