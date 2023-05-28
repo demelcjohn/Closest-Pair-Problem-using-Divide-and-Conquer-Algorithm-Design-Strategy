8a1d19b641eaf48ae396576efa60ff3dd8082e91
demelcjohn <demal.john007@gmail.com>
2023-05-28 13:21:07 +0530

    added base cases

--- findClosestPair.py
+++ findClosestPair.py
@@ -1,5 +1,10 @@
+from helperFunc import dist
+
+
 def findClosestPair(X, Y):
     n, _ = X.shape
 
     if (n == 2):
-        return
+        return dist(X[0], X[1])
+    if (n == 3):
+        return min(dist(X[0], X[1]), dist(X[0], X[2]).dist(X[2], X[1]))
